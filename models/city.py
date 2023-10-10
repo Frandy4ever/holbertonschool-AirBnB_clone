db1e7a91a341ed3cd49abef3f876ea0a5da7feed
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-10 14:10:14 -0500

    update

--- models/city.py
+++ models/city.py
@@ -1,28 +1,10 @@
-<<<<<<< HEAD
-#!/usr/bin/python
-""" holds class City"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-
-
-class City(BaseModel, Base):
-    """Representation of city """
-    pass
-
-    def __init__(self, *args, **kwargs):
-        """initializes city"""
-        super().__init__(*args, **kwargs)
-=======
 #!/usr/bin/python3
-"""City Class"""
-from models import storage
+"""New class inherit from BaseModel"""
 from models.base_model import BaseModel
-from model.engine.file_storage import FileStorage
 
 
 class City(BaseModel):
-    """City Class Model"""
+    """Class City that inherit from BaseModel"""
+
     state_id = ""
     name = ""
->>>>>>> d554b69a90b56cf03619fa98ce009b9cc7313222
