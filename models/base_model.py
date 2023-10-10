91ef0342034f58a6944310094beac8cbebdcbdaa
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-10 14:06:23 -0500

    update base_model.py file code

--- models/base_model.py
+++ models/base_model.py
@@ -1,61 +1,54 @@
 #!/usr/bin/python3
+"""Create class BaseModel that defines all common
+   attributes/methods for other class
 """
-Contains class BaseModel
-"""
-
+from models import storage
+from uuid import uuid4
 from datetime import datetime
-import models
-from os import getenv
-import uuid
-
-time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
-class BaseModel:
-    """The BaseModel class from which future classes will be derived"""
+class BaseModel(object):
+    """Define class BaseModel."""
+
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            if kwargs.get("created_at", None) and type(self.created_at) is str:
-                self.created_at = datetime.strptime(kwargs["created_at"], time)
-            else:
-                self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
-            else:
-                self.updated_at = datetime.utcnow()
-            if kwargs.get("id", None) is None:
-                self.id = str(uuid.uuid4())
+        """Initialize constructor."""
+        t_form = "%Y-%m-%dT%H:%M:%S.%f"
+        self.updated_at = datetime.today()
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        if len(kwargs) > 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, t_form)
+                else:
+                    if k != "__class__":
+                        self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
-        """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
+        """Representation for name class, id and __dict__.
+
+        Returns:
+            string: [class_name] (id) __dict__
+        """
+        class_name = self.__class__.__name__
+        return '[{}] ({}) {}'.format(class_name, self.id, self.__dict__)
 
     def save(self):
-        """updates the attribute 'updated_at' with the current datetime"""
-        self.updated_at = datetime.utcnow()
-        models.storage.new(self)
-        models.storage.save()
+        """Update public instance attributes with the current datetime."""
+        self.updated_at = datetime.today()
+        storage.save()
 
-    def to_dict(self, save_fs=None):
-        """returns a dictionary containing all keys/values of the instance"""
+    def to_dict(self):
+        """Copy dict od instance for ubdate values.
+
+        Returns:
+            dict: dict with new values and update date modificated
+        """
         new_dict = self.__dict__.copy()
-        if "created_at" in new_dict:
-            new_dict["created_at"] = new_dict["created_at"].strftime(time)
-        if "updated_at" in new_dict:
-            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["created_at"] = self.created_at.isoformat()
         new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
-            del new_dict["_sa_instance_state"]
-        if save_fs is None:
-            if "password" in new_dict:
-                del new_dict["password"]
+
         return new_dict
