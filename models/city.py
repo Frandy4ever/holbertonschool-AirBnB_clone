9de8226380c57a9bdf38a86e44e568fcfd09c8fe
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-10 22:16:50 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """New class inherit from BaseModel"""
 from models.base_model import BaseModel
-from models import storage
-from models.engine.file_storage import FileStorage
 
 
 class City(BaseModel):
-    """Class City that inherit from BaseModel"""
+    """Class representing a city that inherits from BaseModel
+        Attributes:
+            state_id (str): The state ID to which the city belongs
+            name (str): The name of the city
+        """
 
     state_id = ""
     name = ""
