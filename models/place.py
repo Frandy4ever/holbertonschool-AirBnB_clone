1da82f16744ed1ce79f19e3a38ecf0588399e0a5
Courtney <125691956+CourtneyAlv@users.noreply.github.com>
2023-10-10 22:17:39 -0500

    Update place.py

--- models/place.py
+++ models/place.py
@@ -4,7 +4,21 @@ from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Class Place that inherit from BaseModel"""
+    """Class representing place that inherits from BaseModel
+
+        Attributes:
+            city_id (str): ID of city where the place is located
+            user_id (str): ID of user who owns the place
+            name (str): the name of the place
+            description (str): description of the place
+            number_rooms (int): number of rooms
+            number_bathrooms (int): number of bathrooms
+            max_guest (int): max number of guests
+            price_by_night (int): price per night to stay
+            latitude (float): the latitude coordinate
+            longitude (float): the longitude coordinate
+            amenity_ids (list): list of amenity IDs
+        """
 
     city_id = ""
     user_id = ""
