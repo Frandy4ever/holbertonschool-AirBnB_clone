da5205360e65725cb669ae93257f0b22d25ce5d2
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-10 20:29:58 -0500

    update base

--- models/base_model.py
+++ models/base_model.py
@@ -6,17 +6,13 @@ Contains class BaseModel
 from datetime import datetime
 import models
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
-if models.storage_t == "db":
-    Base = declarative_base()
-else:
-    Base = object
+
 
 
 class BaseModel:
