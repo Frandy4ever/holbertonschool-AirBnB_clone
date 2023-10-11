805e994b226e5ebc1ccc6e8dc4dfacb54f6d3767
Frandy G Slueue <frandy4ever@gmail.com>
2023-10-10 22:08:10 -0500

    re-add missing files

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,10 @@
 Contains class BaseModel
 """
 
+
 from datetime import datetime
 import models
 from os import getenv
-from sqlalchemy import Column, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,6 +22,7 @@ class FileStorage:
     __file_path = "file.json"
     # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
+
     def all(self, cls=None):
         """returns the dictionary __objects"""
         if cls is not None:
@@ -31,11 +32,13 @@ class FileStorage:
                     new_dict[key] = value
             return new_dict
         return self.__objects
+    
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
+    
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
@@ -45,6 +48,7 @@ class FileStorage:
             json_objects[key] = self.__objects[key].to_dict(save_fs=1)
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
+    
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
@@ -54,15 +58,18 @@ class FileStorage:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
         except:
             pass
+    
     def delete(self, obj=None):
         """delete obj from __objects if it's inside"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
+    
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+    
     def get(self, cls, id):
         """
         Returns the object based on the class name and its ID, or
@@ -75,6 +82,7 @@ class FileStorage:
             if (value.id == id):
                 return value
         return None
+    
     def count(self, cls=None):
         """
 count the number of objects in storage
